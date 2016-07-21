55a3912fb24fb62b668ac062592b564ffa5165b7
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-07-21 15:44:28 +0300

    Done :)

--- app/redux/components/Counter.jsx
+++ app/redux/components/Counter.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Counter = (props) => {
   return (
     <div>
-      <p>Counter: {props.counter}</p><p onClick={() => { props.onIncrement(1); }}>+</p><p>-</p>
+      <p>Counter: {props.counter}</p>
+      <p onClick={() => { props.onIncrement(1); }}>+</p>
+      <p onClick={() => { props.onDecrement(1); }}>-</p>
     </div>
   );
 };
@@ -11,6 +13,7 @@ const Counter = (props) => {
 Counter.propTypes = {
   counter: React.PropTypes.number,
   onIncrement: React.PropTypes.func,
+  onDecrement: React.PropTypes.func,
 };
 
 export default Counter;
