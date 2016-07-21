e81634e1d7c9e84e65a8f746fa751cdc0286d3bc
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-07-21 15:37:06 +0300

    It works!! :-D

--- app/redux/components/Counter.jsx
+++ app/redux/components/Counter.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 const Counter = (props) => {
   return (
     <div>
-      <p>Counter: {props.counter}</p><p>+</p><p>-</p>
+      <p>Counter: {props.counter}</p><p onClick={() => { props.onIncrement(1); }}>+</p><p>-</p>
     </div>
   );
 };
 
 Counter.propTypes = {
   counter: React.PropTypes.number,
+  onIncrement: React.PropTypes.func,
 };
 
 export default Counter;
--- app/redux/containers/index.js
+++ app/redux/containers/index.js
@@ -1,9 +1,21 @@
 import { connect } from 'react-redux';
 import Counter from '../components/Counter.jsx';
+import { increment, decrement } from '../actions';
 
 function mapStateToProps(state) {
   return { counter: state.get('counter', 99) };
 }
 
-export const CounterContainer = connect(mapStateToProps)(Counter);
+function mapDispatchToProps(dispatch) {
+  return ({
+    onIncrement: (number) => {
+      dispatch(increment(number));
+    },
+    onDecrement: (number) => {
+      dispatch(decrement(number));
+    },
+  });
+}
+
+export const CounterContainer = connect(mapStateToProps, mapDispatchToProps)(Counter);
 
